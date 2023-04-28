8787d21892e4ac7fafb82be6e919222c49c34ffa
roxirore <roxana.rod.rev@gmail.com>
2023-04-28 11:07:30 -0500

    jwt.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,15 +1,19 @@
 const catchAsync = require('../utils/catchAsync');
-const User = require('./../models/users.model')
-
+const User = require('./../models/users.model');
+const bcrypt = require('bcryptjs');
 
 exports.signup = catchAsync (async (req, res, next) => {
 
         
-        const { name, email, password, role } = req.body;
+    const { name, email, password, role } = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const encryptedPassword = await bcrypt.hash(password, salt);
+
     const user = await User.create({
       name,
       email,
-      password,
+      password: encryptedPassword,
       role,
     });
     res.status(201).json({
